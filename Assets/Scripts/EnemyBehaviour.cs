e90bd3000a78542edd24ad968bc5cda4329dcd23
Christopher <christopherheggholmen@gmail.com>
2025-10-03 20:30:05 +0200

    More animations and enemy behaviour

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 
@@ -9,7 +10,8 @@ public class EnemyBehaviour : MonoBehaviour
     private Rigidbody _rb;
 
     [SerializeField] private float walkSpeed = 3;
-    private bool activated;
+    private bool activated, stopVeloctiy;
+    private bool dead, plannedAttack;
     
     void Start()
     {
@@ -34,14 +36,88 @@ public class EnemyBehaviour : MonoBehaviour
 
         if (activated)
         {
-            print("Walking towards player");
-            _rb.linearVelocity = transform.forward;
+            if (!stopVeloctiy)
+            {
+                //print("Walking towards player");
+                _rb.linearVelocity = transform.forward.normalized * walkSpeed;
+            }
+
+            if (PlayerInReach() && !plannedAttack)
+            {
+                plannedAttack = true;
+                var randomDelay = UnityEngine.Random.Range(2, 4);
+                StartCoroutine(AttackDelay(randomDelay));
+            }
         }
+        
+        // Animations
+        if (dead)
+        {
+            _anim.Play("Death");
+        }
+        else if (!activated)
+        {
+            print("Not activated");
+            _anim.Play("Idle");
+        }
+        else if (stopVeloctiy)
+        {
+            print("Attacking");
+            //_anim.Play("Attack");
+        }
+        else if (_rb.linearVelocity.magnitude > walkSpeed * 0.8f)
+        {
+            _anim.Play("Walk");
+        }
+        else // standing still for some reason
+        {
+            print(_rb.linearVelocity.magnitude);
+            Debug.Log("Standing still for some reason");
+            _anim.Play("Idle");
+        }
+    }
+
+    private IEnumerator AttackDelay(float delay)
+    {
+        
+        yield return new WaitForSeconds(delay);
+        if (PlayerInReach())
+        {
+            StartCoroutine(Attack());
+        }
+        else
+        {
+            plannedAttack = false;
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        stopVeloctiy = true;
+        
+        _anim.Play("AttackWindup");
+        
+        var attackWindUp = .8f;
+        yield return new WaitForSeconds(attackWindUp);
+        
+        // Spawn attack box
+        _anim.Play("Attack");
+
+        var attackRecovery = 1f;
+        yield return new WaitForSeconds(attackRecovery);
+        
+        plannedAttack = false;
+        stopVeloctiy = false;
     }
 
     private void Behaviour()
     {
         
+    }
+
+    private bool PlayerInReach()
+    {
+        return true;
     }
     
     private bool CanSeePlayer()
