1544acf4dda5c3ac8e73ca2ccb475e3f95c2abd5
Christopher <christopherheggholmen@gmail.com>
2025-11-06 09:26:29 +0100

    Quick push

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -4,6 +4,11 @@ public class LevelSelector : MonoBehaviour
 {
     void Start()
     {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+        
         transform.GetChild(Random.Range(0, transform.childCount)).gameObject.SetActive(true);
     }
 }
